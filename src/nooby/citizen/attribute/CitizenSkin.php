d9d89c950dadf7efd8066e19f1848be128ddf839
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2024-04-05 13:51:57 -0300

    Update CitizenSkin.php

--- src/nooby/citizen/attribute/CitizenSkin.php
+++ src/nooby/citizen/attribute/CitizenSkin.php
@@ -24,23 +24,15 @@ final class CitizenSkin
 		return new Skin("Standard_CustomSlim", $skin_bytes, "", "geometry.humanoid.custom", "");
 	}
 
-	public static function fromCustomGeometry(string $skinPath, string $skinGeometryPath): Skin
+	/**
+        * USE: fromCustomGeometry("skywars, PluginBase::getDataFolder() . "SkyWars.png", PluginBase::getDataFolder() . "SkyWars.geo.json");
+	**/
+	public static function fromCustomGeometry(string $geometry, string $skinPath, string $skinGeometryPath, string $capePath = ''): Skin
 	{
 		$img = imagecreatefrompng($skinPath);
-		$skin_bytes = "";
-		for ($y = 0; $y < imagesy($img); $y++) {
-			for ($x = 0; $x < imagesx($img); $x++) {
-				$colorant = @imagecolorat($img, $x, $y);
-				$a = ((~($colorant >> 24)) << 1) & 0xff;
-				$r = ($colorant >> 16) & 0xff;
-				$g = ($colorant >> 8) & 0xff;
-				$b = $colorant & 0xff;
-				$skin_bytes .= chr($r) . chr($g) . chr($b) . chr($a);
-			}
-		}
-		imagedestroy($img);
-		$c = json_decode(file_get_contents($skinGeometryPath), true);
-		return new Skin($c['skinId'], $skin_bytes, $c['capeData'], $c['geometryName'], $c['geometryName']);
+		$skin_bytes = self::getSkinDataFromPNG($skinPath);
+		//TODO: how to use skinId????
+		return new Skin($c['skinId'], $skin_bytes, $capePath, 'geometry' . $geometry, file_get_contents($skinGeometryPath));
 	}
 
 	public static function getSkinDataFromPNG(string $path): string
