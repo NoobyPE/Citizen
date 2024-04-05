a2099264db668d3c3ce894b977d4074083fc19d8
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2024-04-05 13:56:12 -0300

    Update CitizenSkin.php
    It's time for cosmetics and we're ready :D

--- src/nooby/citizen/attribute/CitizenSkin.php
+++ src/nooby/citizen/attribute/CitizenSkin.php
@@ -6,35 +6,6 @@ use pocketmine\entity\Skin;
 
 final class CitizenSkin
 {
-	public static function fromDefaultGeometry(string $skinPath): Skin
-	{
-		$img = @imagecreatefrompng($skinPath);
-		$skin_bytes = "";
-		for ($y = 0; $y < imagesy($img); $y++) {
-			for ($x = 0; $x < imagesx($img); $x++) {
-				$colorant = @imagecolorat($img, $x, $y);
-				$a = ((~($colorant >> 24)) << 1) & 0xff;
-				$r = ($colorant >> 16) & 0xff;
-				$g = ($colorant >> 8) & 0xff;
-				$b = $colorant & 0xff;
-				$skin_bytes .= chr($r) . chr($g) . chr($b) . chr($a);
-			}
-		}
-		@imagedestroy($img);
-		return new Skin("Standard_CustomSlim", $skin_bytes, "", "geometry.humanoid.custom", "");
-	}
-
-	/**
-        * USE: fromCustomGeometry("skywars, PluginBase::getDataFolder() . "SkyWars.png", PluginBase::getDataFolder() . "SkyWars.geo.json");
-	**/
-	public static function fromCustomGeometry(string $geometry, string $skinPath, string $skinGeometryPath, string $capePath = ''): Skin
-	{
-		$img = imagecreatefrompng($skinPath);
-		$skin_bytes = self::getSkinDataFromPNG($skinPath);
-		//TODO: how to use skinId????
-		return new Skin($c['skinId'], $skin_bytes, $capePath, 'geometry' . $geometry, file_get_contents($skinGeometryPath));
-	}
-
 	public static function getSkinDataFromPNG(string $path): string
 	{
 		$bytes = "";
@@ -51,5 +22,21 @@ final class CitizenSkin
 		}
 		imagedestroy($img);
 		return $bytes;
+	}
+	
+	public static function fromDefaultGeometry(string $skinPath): Skin
+	{
+		return new Skin("Standard_CustomSlim", self::getSkinDataFromPNG($skinPath), "", "geometry.humanoid.custom", "");
+	}
+
+	/**
+        * USE: fromCustomGeometry("skywars, PluginBase::getDataFolder() . "SkyWars.png", PluginBase::getDataFolder() . "SkyWars.geo.json");
+	**/
+	public static function fromCustomGeometry(string $geometry, string $skinPath, string $skinGeometryPath, string $capePath = ''): Skin
+	{
+		$img = imagecreatefrompng($skinPath);
+		$skin_bytes = self::getSkinDataFromPNG($skinPath);
+		//TODO: how to use skinId????
+		return new Skin($c['skinId'], $skin_bytes, $capePath, 'geometry' . $geometry, file_get_contents($skinGeometryPath));
 	}
 }
